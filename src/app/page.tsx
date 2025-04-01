de85ae0cf0683812d184a1e2be9ccbeb89991cc1
정호원 <jjhw0106@naver.com>
2025-04-02 00:44:19 +0900

    로그인 테스트 화면 생성

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+        <h1 className="text-4xl font-bold mb-8">백오피스 프론트엔드</h1>
+        <p className="text-xl">환영합니다!</p>
+      </div>
+    </main>
+  )
+} 
